dc240d78200a67fae5c3a1a9ed10fbd5863bf0fa
DevaKune <53827543+deva9017@users.noreply.github.com>
2025-02-11 18:03:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,13 @@ resource "aws_s3_bucket" "lambda_code" {
 }
 
 resource "aws_lambda_function" "app_lambda" {
-  function_name    = "hello-world-lambda-${terraform.workspace}"
+  function_name    = "hello-world-lambda-${var.environment}"
+  s3_bucket       = "lambda-code-bucket-${var.environment}"
+  s3_key          = "lambda_function.zip"
   role            = aws_iam_role.lambda_role.arn
-  runtime         = "nodejs18.x"
   handler         = "index.handler"
-  
-
-  s3_bucket       = data.aws_s3_bucket.lambda_code_bucket.id
-  s3_key          = aws_s3_object.lambda_zip.key  
-
-  source_code_hash = null_resource.trigger.id # Force redeployment
-
-  environment {
-    variables = {
-      ENV = terraform.workspace
-    }
-  }
+  runtime         = "nodejs18.x"
 }
-
 resource "null_resource" "trigger" {
   triggers = {
     redeploy = timestamp()
