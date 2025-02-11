8d63c44b69490a28b8d49cf47924ff41ae480f1a
DevaKune <53827543+deva9017@users.noreply.github.com>
2025-02-11 16:20:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket986"
-    key            = "infra/terraform.tfstate"
+    key            = "testinfra/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
