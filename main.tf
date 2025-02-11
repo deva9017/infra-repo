6a53dc2ef0cb1f925126e940eb9922905cc78818
DevaKune <53827543+deva9017@users.noreply.github.com>
2025-02-11 18:51:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ resource "aws_lambda_function" "app_lambda" {
   runtime         = "nodejs18.x"
  }
 
-resource "aws_api_gateway_rest_api" "app_api" {
-  name        = "app-api-${local.environment}"
-  description = "API Gateway for Lambda app"
-}
-
 module "auth" {
   source      = "./modules/auth"
   environment = local.environment
