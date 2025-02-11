88c581322908948a88bef5e6c668850fe4a3fd56
DevaKune <53827543+deva9017@users.noreply.github.com>
2025-02-11 16:25:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,23 @@ resource "aws_s3_bucket" "lambda_code" {
 }
 
 resource "aws_lambda_function" "app_lambda" {
-  function_name = "my-app-lambda-${local.environment}"
-  s3_bucket     = aws_s3_bucket.lambda_code.bucket
-  s3_key        = "lambda.zip"
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = aws_iam_role.lambda_role.arn
+  function_name    = "hello-world-lambda-${terraform.workspace}"
+  role            = aws_iam_role.lambda_role.arn
+  runtime         = "nodejs18.x"
+  handler         = "index.handler"
+
+  s3_bucket       = aws_s3_bucket.lambda_code.bucket
+  s3_key          = aws_s3_object.lambda_zip.key
+
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      ENV = terraform.workspace
+    }
+  }
 }
+
 
 resource "aws_api_gateway_rest_api" "app_api" {
   name        = "app-api-${local.environment}"
