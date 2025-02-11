1d4cce81189dd9302baae40eb696e2830b6a057b
DevaKune <53827543+deva9017@users.noreply.github.com>
2025-02-11 16:59:51 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,8 @@
+/*
 resource "aws_s3_bucket" "lambda_code" {
   bucket = "lambda-code-bucket19-${terraform.workspace}"
 }
-
+*/
 resource "aws_s3_object" "lambda_zip" {
   bucket = aws_s3_bucket.lambda_code.bucket
   key    = "lambda_function.zip"
